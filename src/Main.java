8c2af71682d9a07fe3b3a0eb5caaf7d7348bf496
Бегали <b_mashrapov@mail.ru>
2022-11-09 17:05:29 +0100

    изменения

--- src/Main.java
+++ src/Main.java
@@ -5,28 +5,59 @@ public class Main {
         b [0] = 1;
         b [1]=2;
         b[2]=3;
-        System.out.print(b[0] + ", " + b[1] + ", " + b[2]); // ex.2.1
-        System.out.println();
-        for (int i = 0; i < 3; i++) {//ex 4
-            if (b[i] % 2 != 0) {b[i]+=1;}
-            System.out.print(b[i] + " ");
-            System.out.println();
+        for (int i = 0; i <= b.length-1; i++) {
+            if (i != b.length-1) {
+                System.out.print(b[i] + ", ");
+            } else
+                System.out.print(b[i]);// ex.2.1
         }
+        System.out.println();
         //ex 1.2
         double [] a = {1.57, 7.6554, 9.986};
-            System.out.print(a[0] + ", " + a[1] + ", " + a[2]); // ex 2.2
+        for (int i = 0; i <= a.length-1; i++) {
+            if (i != a.length-1) {
+                System.out.print(a[i] + ", ");
+            } else System.out.print(a[i]);
+        }// ex 2.2
         System.out.println();
         //ex 1.3
         int [] roadLenght = {150, 300, 450};
-        System.out.print(roadLenght[0] + ", " + roadLenght[1] + ", " + roadLenght[2]);// ex 2.3
+        for (int i = 0; i <= roadLenght.length-1; i++) {
+            if (i != roadLenght.length-1) {
+                System.out.print(roadLenght[i] + ", ");
+            } else
+                System.out.print(roadLenght[i]);// ex.2.1
+        }
         System.out.println();
-        System.out.print(b[2] + ", " + b[1] + ", " + b[0]); // ex.3.1
+        for (int i = b.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(b[i] + ", ");
+            } else
+                System.out.print(b[i]);
+        } // ex.3.1
         System.out.println();
-        System.out.print(a[2] + ", " + a[1] + ", " + a[0]); // ex 3.2
+        for (int i = a.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(a[i] + ", ");
+            } else System.out.print(a[i]);
+        } // ex 3.2
         System.out.println();
-        System.out.print(roadLenght[2] + ", " + roadLenght[1] + ", " + roadLenght[0]);// ex 3.3
+        for (int i = roadLenght.length - 1; i >= 0; i--) {
+            if (i != 0) {
+                System.out.print(roadLenght[i] + ", ");
+            } else
+                System.out.print(roadLenght[i]);
+        }// ex 3.3
         System.out.println();
         //ex 4
+        for (int i = 0; i < 3; i++) {
+            if (b[i] % 2 != 0) {
+                b[i] += 1;}
+            if (i != b.length-1) {
+                System.out.print(b[i] + ", ");
+            } else
+                System.out.print(b[i]);
+        }
 
     }
 }
